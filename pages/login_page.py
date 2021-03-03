0328fcef82d868a338b461bea495b3ea82c6ef7e
Victor Alexeev <avman900@gmail.com>
2021-03-03 20:55:18 +0300

    fix bugs in checks of login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-from selenium.common.exceptions import NoSuchElementException
 
 
 class LoginPage(BasePage):
@@ -10,18 +9,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.current_url, "\"login\" is not presented"
+        assert "login" in self.browser.current_url, "\"login\" is not presented in current URL"
 
     def should_be_login_form(self):
-        try:
-            self.is_element_present(*LoginPageLocators.LOGIN_FORM)
-        except (NoSuchElementException):
-            return False
-        assert True, "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        try:
-            self.is_element_present(*LoginPageLocators.REGISTER_FORM)
-        except (NoSuchElementException):
-            return False
-        assert True, "Register form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
